3eaedb1ca87d4c45a9044b6370882e05644864ec
menna-hussien <31513435+menna-hussien@users.noreply.github.com>
2020-05-03 15:51:45 +0200

    save all figures in one
    cloned function to save all matplot figures int just one figure, need to be configured on our project

---
+++ Backend/save_image.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+def show_images(images, cols=1, titles=None):
+    """Display a list of images in a single figure with matplotlib.
+
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+
+    cols (Default = 1): Number of columns in figure (number of rows is
+                        set to np.ceil(n_images/float(cols))).
+
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
+    assert ((titles is None) or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1, n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images / float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    ##added by me :D just to save the image in the the static folder
+    plt.savefig(os.path.join(globals.app.static_folder, "output.png"))
+
+    # plt.show()
