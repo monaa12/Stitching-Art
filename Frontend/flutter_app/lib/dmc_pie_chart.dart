1c2a635b9c87cc2d536e28141e1e1dcc75f50d28
Mona Mahmoud <monamahmoud1997@gmail.com>
2020-05-13 00:26:50 +0300

    Update dmc_pie_chart.dart

--- Frontend/flutter_app/lib/dmc_pie_chart.dart
+++ Frontend/flutter_app/lib/dmc_pie_chart.dart
@@ -16,23 +16,7 @@ class _DMCChartState extends State<DMCChart> {
   double download=0.0;
   File f;
   var imageUrl="http://127.0.0.1:5000/dmc_pie_chart";
-  Widget getImageWidget() {
-    if (f != null) {
-      return Image.file(
-        f,
-       // width: 500,
-       // height: 500,
-       // fit: BoxFit.cover,
-      );
-    } else {
-      return Image.network(
-        "assets/loading.jpg",
-      //  width: 500,
-      //  height: 500,
-       // fit: BoxFit.cover,
-      );
-    }
-  }
+
   Future<void> downloadDMCPieChart(imageUrl) async
   {
     try{
@@ -40,7 +24,7 @@ class _DMCChartState extends State<DMCChart> {
       var dir=await getApplicationDocumentsDirectory();
       String fileName=imageUrl.substring(imageUrl.lastIndexOf("/")+1);
       f=File("${dir.path}/$fileName");
-      await dio.download(imageUrl, "${dir.path}/$fileName" /*"${dir.path}/gridedddd.png"*/,onReceiveProgress: (rec,total){
+      await dio.download(imageUrl, "${dir.path}/$fileName" ,onReceiveProgress: (rec,total){
         setState(() {
           downloading=true;
           download=(rec/total)*100;
@@ -77,21 +61,7 @@ class _DMCChartState extends State<DMCChart> {
         backgroundColor: Colors.red[700],
       ),
       body:
-     // Center(
-     //   child:getImageWidget(),
-      //),
-      /*Column(
-        // child: Center(
-        //child: Image.file(f,height: 600, width: 600,fit: BoxFit.cover),
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          getImageWidget(),
-          //Image.file(f, height: 600, width: 600, fit: BoxFit.cover),
 
-        ],
-      ),
-      */
       Center(
         child: downloading?Container(
           height: 250,
@@ -118,7 +88,7 @@ class _DMCChartState extends State<DMCChart> {
 
       floatingActionButton:  FloatingActionButton(
         onPressed: () {
-          //Navigator.push(context, MaterialPageRoute(builder: (context) => Chart()));
+
           Navigator.pushNamed(context, '/');
         },
         child: Icon(
