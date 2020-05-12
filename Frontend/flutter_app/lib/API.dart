3ae756f235a4001d58bc25d68a743adfa026f412
Mona Mahmoud <monamahmoud1997@gmail.com>
2020-05-12 23:40:54 +0300

    Update API.dart

--- Frontend/flutter_app/lib/API.dart
+++ Frontend/flutter_app/lib/API.dart
@@ -1,51 +1,15 @@
-//import 'dart:html';
-import 'package:flutter/material.dart';
+//needed packages for sending requests
 import 'package:path/path.dart';
 import 'package:dio/dio.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:io';
-import 'Gridded_image.dart';
 
-//  final GlobalKey<ScaffoldState> _scaffoldstate =new GlobalKey<ScaffoldState>();
-/*void uploadFile(filePath) async {
-  // Get base file name
-  String fileName = basename(filePath.path);
-  print("File base name: $fileName");
-
-  try {
-    FormData photo =
-    new FormData.from({"photo": new UploadFileInfo(filePath, fileName)});
-    Response response = await Dio().post("http://10.0.2.2:5000/", data: photo);
-    print("File upload response: $response");
-    //   _showBarMsg(response.data['message']);
-  } catch (e) {
-    print("Exception Caught: $e");
-  }
-}
-
- */
-class APIs extends StatefulWidget {
-  @override
-  _APIsState createState() => _APIsState();
-}
-
-class _APIsState extends State<APIs> {
-  @override
-
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-
-
+//function to upload image to server
 Future <void> uploadFile(filePath) async{
   // Get base file name
   String fileName = basename(filePath.path);
   print("File base name: $fileName");
   try{
     FormData photo = new FormData.fromMap({"photo" : await MultipartFile.fromFile(filePath.path,filename: fileName)});
-    Response response = await Dio().post("http://10.0.2.2:5000/", data: photo);
+    Response response = await Dio().post("http://127.0.0.1:5000/", data: photo);
     print("File upload response: $response");
   }
   catch(e)
@@ -53,79 +17,24 @@ Future <void> uploadFile(filePath) async{
     print("Exception Caught: $e");
   }
 }
+//function to upload number of stitches to server
+Future <void> uploadStitches(numStitches,numColors) async{
+try {
 
-void uploadStitches(number,numColors) async{
-  try {
-    // FormData formData = new FormData.from({
-    //   "width": number
-    // });
-    Dio dio = new Dio();
-    //Response res = await Dio().post(
-    //    "http://10.0.2.2:5000/params", data: {"width": number, "height": 8});
-    //print("value: $number");
-    //List<Response> response = await Future.wait([dio.post("/info"), dio.get("/token")]);
-    List<Response> response = await Future.wait([dio.post("http://192.168.1.3:5000/params", data: {"width": number, "height": numColors}),
-      dio.get("http://192.168.1.3:5000/params")]);
-    print(response[1].data.toString());
-    double height=response[1].data["height"];
-    double width=response[1].data["width"];
-    print(height);
-    print(width);
-    //Response res2 = await Dio().get("http://10.0.2.2:5000/params");
-    //print(res2.data);
-  }
-  catch(e){
-    print("Exception Caught: $e");
-  }
+  Dio dio = new Dio();
+  List<Response> response = await Future.wait([dio.post("http://127.0.0.1:5000/params", data: {"width": numStitches, "height": numColors}),
+    dio.get("http://127.0.0.1:5000/params")]);
 }
+catch(e){
+  print("Exception Caught: $e");
+}
+}
+//function to upload label if object detection option is selected to server
 Future <void> uploadLabel(object) async{
   try {
-    Response resp = await Dio().post(
-        "http://10.0.2.2:5000/automatic_crop", data: {"label": object});
-    print("the object is: $object");
+    Response resp = await Dio().post("http://127.0.0.1:5000/automatic_crop", data: {"label": object});
   }
   catch(e){
-    print("Exception Caught: $e");
+  print("Exception Caught: $e");
   }
 }
-void uploadLabelImage(object,filePath) async{
-  String fileName = basename(filePath.path);
-  print("File base name: $fileName");
-  try{
-    FormData photoLabel = new FormData.fromMap({"label": object,"photo" : await MultipartFile.fromFile(filePath.path,filename: fileName)});
-    Response response = await Dio().post("http://10.0.2.2:5000/", data: photoLabel);
-    print("File upload response: $response");
-    print("the object is: $object");
-  }
-  catch(e)
-  {
-    print("Exception Caught: $e");
-  }
-
-}
-/*Future <void> downloadImage() async{
- // String savePath="./grided.png";
-  final imageUrl="http://10.0.2.2:5000/gridded";
-  Dio dio=Dio();
-  try{
-    var directory=await getApplicationDocumentsDirectory();
-    await dio.download(imageUrl, "${directory.path}/grided.png", onReceiveProgress: (rec,total){
-      print("Rec: $rec , Total: $total");
-    });
-  }
-  catch(e){
-    print(e);
-  }
-  //Response image= await Dio().download("http://10.0.2.2:5000/gridded", savePath);
-
-
-  //File f;
-  //f=File("${dir.path}/grided.png");
-}
-
- */
-
-
-//void _showBarMsg(String msg) {
-// scaffoldstate.currentState.showSnackBar(new SnackBar(content: new Text(msg)));
-//}
