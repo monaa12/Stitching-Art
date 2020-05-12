f2e732f2f7606e4058f53ec2f671001c60926e83
Mona Mahmoud <monamahmoud1997@gmail.com>
2020-05-12 23:40:38 +0300

    Update num_stitches.dart

--- Frontend/flutter_app/lib/num_stitches.dart
+++ Frontend/flutter_app/lib/num_stitches.dart
@@ -1,23 +1,19 @@
+//needed packages
 import 'package:flutter/material.dart';
+//needed .dart files
 import 'package:flutterapp/API.dart';
-//import 'package:flutter/material.dart';
-//import 'API.dart';
-import 'package:flutterapp/Gridded_image.dart';
-import 'package:flutterapp/main.dart';
 
-class output extends StatefulWidget {
+class StitchesColors extends StatefulWidget {
   @override
-  _outputState createState() => _outputState();
+  _StitchesColorsState createState() => _StitchesColorsState();
 }
 
-class _outputState extends State<output> {
-  final TextEditingController textCont= new TextEditingController ();
-  final TextEditingController controller= new TextEditingController ();
-  final TextEditingController controller2= new TextEditingController ();
-  String result="";
-  String result2="";
-  String res="";
- Future <List<double>>dim;
+class _StitchesColorsState extends State<StitchesColors> {
+
+  final TextEditingController StitchesController= new TextEditingController ();
+  final TextEditingController Colorscontroller= new TextEditingController ();
+  String colors="";
+  String stitches="";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +45,7 @@ class _outputState extends State<output> {
                 style: TextStyle(
                   fontWeight: FontWeight.bold,
                   fontSize: 25.0,
-                 // color : Colors.red[600],
+                  // color : Colors.red[600],
                 ),
               ),
             ),
@@ -62,8 +58,6 @@ class _outputState extends State<output> {
                   flex:1,
                   child: Container(
                     alignment:Alignment.centerLeft,
-                    //height: 50,
-                    //width: 400,
                     padding: EdgeInsets.all(10.0),
                     color: Colors.white70,
                     child: Text('Number of Stitches of Width:',
@@ -76,103 +70,44 @@ class _outputState extends State<output> {
                     ),
                   ),
                 ),
-               
-              Expanded(
-                flex:1,
-                child: Container(
-
-                  padding: EdgeInsets.all(10.0),
-                  //color: Colors.red[200],
-                  child:          new TextField(
-                    decoration: new InputDecoration(
-                        hintText: "stitches"
-                    ),
-                    onSubmitted: (String str){
-                      setState(() {
-                        res = str;
-                        //uploadFile(res);
-                      });
-                      textCont.text="";
-                    },
-                    controller: textCont,
-
-                  ),
-                ),
-              )
-                ],
-                ),
-          ),
-        Expanded(
-          flex:1,
-          child: Column(
-            children: <Widget>[
-            Expanded(
-            flex:1,
-            child: Container(
-              alignment:Alignment.centerLeft,
-              padding: EdgeInsets.all(10.0),
-              color: Colors.white70,
-              child: Text('Number of image colors:',
-                style: TextStyle(
-                  fontSize:20.0,
-                  fontWeight: FontWeight.normal,
-                  color : Colors.red[600],
-
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            flex:1,
-            child: Container(
-
-              padding: EdgeInsets.all(10.0),
-              //color: Colors.red[200],
-              child:          new TextField(
-                decoration: new InputDecoration(
-                    hintText: "colors"
-                ),
-                onSubmitted: (String str){
-                  setState(() {
-                    result = str;
-                  });
-                  controller2.text="";
-                },
-                controller: controller2,
-              )
-              ,
-            ),
-          ),
-        ],
-          ),
-        ),
-          MaterialButton(
-               color:Colors.red[600],
-
-              child: Text(
-                "Show expected output dimensions"
-                //Icons.add_a_photo
-              ),
-              //backgroundColor: Colors.red,
-              onPressed: () {
-              dim= uploadStitches(res,result);
-               // _showOptions(context);
-              }),
-          
-          Expanded(
-            flex:1,
-            child: Row(
-              children: <Widget>[
                 Expanded(
                   flex:1,
                   child: Container(
 
                     padding: EdgeInsets.all(10.0),
-                    color: Colors.red[600],
-                    child: Text('width:',
+                    child:          new TextField(
+                      decoration: new InputDecoration(
+                          hintText: "stitches"
+                      ),
+                      onSubmitted: (String str){
+                        setState(() {
+                          stitches = str;
+                        });
+                        StitchesController.text=stitches;
+                      },
+                      controller: StitchesController,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex:1,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex:1,
+                  child: Container(
+                    alignment:Alignment.centerLeft,
+                    padding: EdgeInsets.all(10.0),
+                    color: Colors.white70,
+                    child: Text('Number of image colors:',
                       style: TextStyle(
                         fontSize:20.0,
-                        fontWeight: FontWeight.bold,
+                        fontWeight: FontWeight.normal,
+                        color : Colors.red[600],
+
                       ),
                     ),
                   ),
@@ -182,64 +117,35 @@ class _outputState extends State<output> {
                   child: Container(
 
                     padding: EdgeInsets.all(10.0),
-                    //color: Colors.red[200],
-                    child: Text(
-                      dim.toString() ,
-                    )
-                    ,
-                  ),
-                ),
-                //Text('width'),
-                //Text('length'),
-                Expanded(
-                  flex:1,
-                  child: Container(
-
-                    padding: EdgeInsets.all(10.0),
-                    color: Colors.red[600],
-                    child: Text('length:',
-                      style: TextStyle(
-                        fontSize:20.0,
-                        fontWeight: FontWeight.bold,
-                      ) ,
+                    child:          new TextField(
+                      decoration: new InputDecoration(
+                          hintText: "colors"
+                      ),
+                      onSubmitted: (String str){
+                        setState(() {
+                          colors = str;
+                        });
+                        Colorscontroller.text=colors;
+                      },
+                      controller: Colorscontroller,
                     ),
+
                   ),
                 ),
-                Expanded(
-                  flex:1,
-                  child: Container(
-
-                    padding: EdgeInsets.all(10.0),
-                    //color: Colors.red[200],
-                    child:new Text(
-                      dim.toString(),
-                    ),
-                  ),
-                ),
-
               ],
             ),
           ),
-
-          //////////////////haiodaaa
           Expanded (
             flex:2,
             child: Container(
-
             ),
           ),
         ],
-
       ),
-
-
       floatingActionButton:  FloatingActionButton(
         onPressed: () {
-          //Navigator.push(context, MaterialPageRoute(builder: (context) => Gridded()));
           Navigator.pushNamed(context, '/gridded');
-          //uploadStitches(res,result);
-          /// hanb3t hena el number of colors
-          //uploadStitches(result);
+          uploadStitches(stitches,colors);
         },
         child: Icon(
             Icons.done
